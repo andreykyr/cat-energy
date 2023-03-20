2f8ae6cbcf1c353943e36ac72bbcc9acf46af6e3
andrey-work <kyrov95@gmail.com>
2023-03-20 16:40:42 +0300

    сделал слайдер для мобильных устройств

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,6 @@
+
+// MENU
+
 const mobileMenu = document.querySelector('.main-nav');
 const toggleBtn = mobileMenu.querySelector('.main-nav__toggle-btn');
 const menuList = mobileMenu.querySelector('.main-nav__list');
@@ -11,4 +14,32 @@ toggleBtn.addEventListener('click', function() {
   menuList.classList.toggle('main-nav__list--open-animation');
 });
 
-console.log(mobileMenu, toggleBtn, menuList);
+//SLIDER
+
+const sliderBlock = document.querySelector('.example-interactive');
+const sliderRail = sliderBlock.querySelector('.diet-slider__rail');
+const sliderBtn = sliderBlock.querySelector('.diet-slider__btn');
+
+const afterImg = sliderBlock.querySelector('.example-interactive__img--after');
+const beforeImg = sliderBlock.querySelector('.example-interactive__img--before');
+
+sliderRail.addEventListener('click', function() {
+  if (sliderBtn.classList.contains('diet-slider__btn--before')) {
+    sliderBtn.classList.remove('diet-slider__btn--before');
+    sliderBtn.offsetWidth = sliderBtn.offsetWidth;
+    sliderBtn.classList.add('diet-slider__btn--after');
+    afterImg.classList.remove('example-interactive__img--inactive');
+    beforeImg.classList.add('example-interactive__img--inactive');
+  } else {
+    sliderBtn.classList.remove('diet-slider__btn--after');
+    sliderBtn.offsetWidth = sliderBtn.offsetWidth;
+    sliderBtn.classList.add('diet-slider__btn--before');
+    beforeImg.classList.remove('example-interactive__img--inactive');
+    afterImg.classList.add('example-interactive__img--inactive');
+  }
+})
+
+buttonAfter.addEventListener('click', function() {
+  buttonBefore.classList.add('diet-slider__btn--inactive');
+  buttonAfter.classList.remove('diet-slider__btn--inactive');
+})
