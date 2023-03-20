8024895d249b6b2919a975d45d839e69489376f4
andrey-work <kyrov95@gmail.com>
2023-03-20 10:12:02 +0300

    добавил скрипт для мобильного меню

---
+++ source/js/script.js
@@ -0,0 +1,13 @@
+const mobileMenu = document.querySelector('.main-nav');
+const toggleBtn = mobileMenu.querySelector('.main-nav__toggle-btn');
+const menuList = mobileMenu.querySelector('.main-nav__list');
+
+toggleBtn.classList.add('main-nav__toggle-btn--open-menu');
+menuList.classList.add('main-nav__list--menu-closed');
+
+toggleBtn.addEventListener('click', function() {
+  toggleBtn.classList.toggle('main-nav__toggle-btn--open-menu');
+  menuList.classList.toggle('main-nav__list--menu-closed');
+});
+
+console.log(mobileMenu, toggleBtn, menuList);
