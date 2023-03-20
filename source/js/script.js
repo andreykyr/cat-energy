94cb845eb7b581a4edc7dcd84ff71b8f0a58a09f
andrey-work <kyrov95@gmail.com>
2023-03-20 14:18:39 +0300

    добавил анимированное открытие меню

--- source/js/script.js
+++ source/js/script.js
@@ -8,6 +8,7 @@ menuList.classList.add('main-nav__list--menu-closed');
 toggleBtn.addEventListener('click', function() {
   toggleBtn.classList.toggle('main-nav__toggle-btn--open-menu');
   menuList.classList.toggle('main-nav__list--menu-closed');
+  menuList.classList.toggle('main-nav__list--open-animation');
 });
 
 console.log(mobileMenu, toggleBtn, menuList);
